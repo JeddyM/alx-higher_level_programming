a0a2f113e98a87ae146aafcd3c1e9abd33b78ac6
Jeddy Mweru <jeddymweru@gmail.com>
2022-06-30 21:36:23 +0000

    Fixing syntax errors in task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     count = len(sys.argv) - 1
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit (1)
+        sys.exit(1)
     #using a dictionary to map sign to function
     funcs = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(funcs.keys()):
-        print(" Unknown operator. Available operators: +, -, * and /")
-        sys.exit (1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
 
     a = int(sys.argv[1])
