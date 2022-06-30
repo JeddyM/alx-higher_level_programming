83c841fd5c7a75efc8ad3b0dfba71a829e61ef82
Jeddy Mweru <jeddymweru@gmail.com>
2022-06-30 14:35:45 +0000

    Fixing errors in task 2 and 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     position = len(sys.argv) - 1
     if position == 0:
         print("0 arguments.")
-    elif position == 1
+    elif position == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(position))
