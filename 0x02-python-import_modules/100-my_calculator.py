e61d542fb1740b6a971d98f94c11f2999feb43e1
Jeddy Mweru <jeddymweru@gmail.com>
2022-06-30 21:04:14 +0000

    Fixing nameerror in task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1, sys
+    from calculator_1 import *
+    import sys
 
     count = len(sys.argv) - 1
+    op = sys.argv[2]
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit (1)
     #using a dictionary to map sign to function
-    funcs = {"+":add, "-": sub, "*": mul, "/": div}
+    funcs = {"+": add, "-": sub, "*": mul, "/": div}
     if op not in list(funcs.keys()):
         print(" Unknown operator. Available operators: +, -, * and /")
         sys.exit (1)
 
 
     a = int(sys.argv[1])
-    op = sys.argv[2]
     b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, op, b, funcs[c](a, b)))
