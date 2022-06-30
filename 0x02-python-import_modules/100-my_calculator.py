1acae6939c6850e9b418c009a76be23866d9f10f
Jeddy Mweru <jeddymweru@gmail.com>
2022-06-30 21:23:35 +0000

    Fixing errors in index errors and task 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,17 +4,16 @@ if __name__ == "__main__":
     import sys
 
     count = len(sys.argv) - 1
-    op = sys.argv[2]
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit (1)
     #using a dictionary to map sign to function
     funcs = {"+": add, "-": sub, "*": mul, "/": div}
-    if op not in list(funcs.keys()):
+    if sys.argv[2] not in list(funcs.keys()):
         print(" Unknown operator. Available operators: +, -, * and /")
         sys.exit (1)
 
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, op, b, funcs[op](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, funcs[sys.argv[2]](a, b)))
