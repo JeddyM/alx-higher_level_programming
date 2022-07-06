476af494924be9530adccf5a02527dc28224dc42
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-06 21:44:37 +0000

    Fixing syntax errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     '''A function that returns a key with the biggest integer value.'''
-    if not isinstance(a_dictionary, dict) not len(a-dictionary) == 0:
+    if not isinstance(a_dictionary, dict) not len(a_dictionary) == 0:
         return None
     my_list = list(a_dictionary.keys())[0]
     big_int = a_dictionary[my_list]
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    '''A function that returns a new dictionary with all values multiplied by 2'''
- i   new_dict = {}
+    '''Returns a new dictionary with all values multiplied by 2'''
+    new_dictionary = {}
     for i in a_dictionary:
-        new_dict[i] = a_dictionary[i] * 2
-    return new_dict
+        new_dictionary[i] = a_dictionary[i] * 2
+    return new_dictionary
