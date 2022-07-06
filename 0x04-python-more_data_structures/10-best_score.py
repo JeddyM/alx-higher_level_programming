ed2de98383d37f3df5df7c3ec0ee156d81be084f
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-06 21:21:26 +0000

    Task 5, 6, 7, 8, 9 and 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    '''A function that returns a key with the biggest integer value.'''
+    if not isinstance(a_dictionary, dict) not len(a-dictionary) == 0:
+        return None
+    my_list = list(a_dictionary.keys())[0]
+    big_int = a_dictionary[my_list]
+    for n, i in a_dictionary.items():
+        if i > big_int:
+            big_int = i
+            my_list = n
+    return my_list
---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    '''A function that prints a dictionary by ordered keys'''
+    for i in sorted(a_dictionary.keys()):
+        print("{}: {}".format(i, a_dictionary[i]))
