f44f9b106fbf9b2b7b03011539cf3973e96aa169
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-06 21:54:57 +0000

    Fixing syntax errors

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    '''Function that computes the square value of all integers of a matrix.'''
-    return ([list(map(lambda num: num * num, i)) for i in matrix])
+    '''Computes the square value of all integers of a matrix.'''
+    return ([list(map(lambda num: num ** 2, i)) for i in matrix])
