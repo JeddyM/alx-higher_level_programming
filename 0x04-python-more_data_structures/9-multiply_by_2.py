94c00e633fca92e6d51f4fa76947217f5ba40741
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-06 21:34:45 +0000

    Fixing syntax errors

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     '''A function that returns a new dictionary with all values multiplied by 2'''
-    new_dict = {}
+ i   new_dict = {}
     for i in a_dictionary:
         new_dict[i] = a_dictionary[i] * 2
-    return new_dict 
+    return new_dict
