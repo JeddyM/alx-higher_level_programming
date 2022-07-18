129086fe68f71295d573752f9555060b54e0297a
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-18 17:21:02 +0000

    Fixing syntax errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,9 +7,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             division = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-             print("division by 0")
-             division = 0
-
+            print("division by 0")
+            division = 0
         except TypeError:
             print("wrong type")
             division = 0
