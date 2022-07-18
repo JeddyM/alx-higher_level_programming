e52d2ae0b944076a981cd22e27f5b6d127dc32ec
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-18 17:10:22 +0000

    Fixing syntax errors

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def raise_exception():
-    '''A function that raises a type exception.'''
-    raise TypeError 
+    '''A function that raises a type exception'''
+    raise TypeError
