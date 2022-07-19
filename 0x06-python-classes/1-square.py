efd52541acc891782f864b3d5c312c948b3b7d31
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-19 17:44:14 +0000

    Readme and first 2 tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    '''class Square'''
+    def __init__(self, size):
+        '''Initialize the square class
+        Args:
+        size(int): private instance attribute
+        '''
+        self.__size = size
