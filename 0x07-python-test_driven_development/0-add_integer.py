cd8b0a2a7ce1ce30c9630b261ba84270bc6fdc04
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-26 22:03:31 +0000

    Task 0 & 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python3
+'''Module for the add_integer function'''
 
-def add_integer(a, b):
-    return a + b
+
+def add_integer(a, b=98):
+    '''A function that adds 2 integers
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Raises:
+        TypeError: if the inputs are neither integers or float
+
+    Return:
+        sum of a and b
+    '''
+    if isinstance(a, (int, float)) is False:
+        raise TypeError('a must be an integer')
+    if isinstance(b, (int, float)) is False:
+        raise TypeError('b must be an integer')
+    return int(a + b)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""""""
+
+def matrix_divided(matrix, div):
+    """Function that divides all elements of a matrix
+    Args:
+        matrix(int):matrix
+        div(int, float):integer dividing elements
+    Raises:
+        TypeError: if matrix item is not integer or float
+        TypeError: if rows of matrix is not same size
+        TypeError: if div not float or integer
+        ZeroDivisionError: if div  eequal to zero
+    Returns:
+        A new matrix
+    """
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
+    if not div:
+        raise ZeroDivisionError('division by zero')
+
+    new_matrix= []
+    new_nums= []
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError('Each row of the matrix must have the same size')
+        if not all(row) and all(matrix[row]) is False:
+            raise TypeError(
+                'matrix must be a matrix (list of lists) of integers/floats')
+        for num in row:
+            if isinstance(num, (int, float)) is False:
+                raise TypeError(
+                    'matrix must be a matrix (list of lists) of integers/floats')
+            new_nums.append(round(num / div, 2))
+        new_matrix.append(new_nums)
+        new_nums = []
+    return new_matrix
