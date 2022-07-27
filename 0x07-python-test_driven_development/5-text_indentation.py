80bb7d167bca51ad74bc3ec0361980f4ebcb49a5
Jeddy Mweru <jeddymweru@gmail.com>
2022-07-27 17:04:24 +0000

    Task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module for text_indentation(text) function"""
+
+
 def text_indentation(text):
     """Prints a text with 2 new lines after these characters: ., ? and :
     Args:
@@ -8,4 +10,13 @@ def text_indentation(text):
     if isinstance(text, str) is False:
         raise TypeError("text must be a string")
 
-
+    c = ""
+    for i in text:
+        if i in ['.', '?', ':']:
+            c += i
+            print("{:s}\n".format(c.strip()))
+            c = ""
+        else:
+            c += i
+    if c != "":
+        print(c.strip(), end="")
