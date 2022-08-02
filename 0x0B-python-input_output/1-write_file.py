9b1daf49ce8ae3cd966edc50f6952293ba5b356e
Jeddy Mweru <jeddymweru@gmail.com>
2022-08-02 15:40:24 +0000

    Task 0 to 2

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""1-write_file.py Module"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) & returns the number
+    of characters written:
+        Args:
+            filename: file name
+            text: string to add
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        file_content = f.write(str(text))
+        return file_content
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""2-append_write.py module"""
+
+
+def append_write(filename="", text=""):
+    """appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    Args:
+        filename: file name
+        text(str): text to append
+    Returns:
+        number of characters added
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
