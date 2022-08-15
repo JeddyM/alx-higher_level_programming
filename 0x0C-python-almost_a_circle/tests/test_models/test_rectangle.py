35911bc12255604232c1592cd369b352e1e53e0d
Jeddy Mweru <jeddymweru@gmail.com>
2022-08-15 21:52:00 +0000

    More tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,6 +19,8 @@ class TestRectangle(unittest.TestCase):
         1st arg a string
         3rd is not an int
         4th args not an int
+        width = 0
+        height = 0
         respectively
         """
         self.assertIsInstance(Rectangle(9, 4), Base)
@@ -34,8 +36,82 @@ class TestRectangle(unittest.TestCase):
             Rectangle("8", 1)
         with self.assertRaisesRegex(TypeError, "x must be an integer"):
             Rectangle(8, 1, "3")
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Rectangle(8, 1, 6, "10")
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(0, 2)
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(2, 0)
+
+
+    def test_area(self):
+        """Test area"""
+        Rect = Rectangle(5, 3)
+        self.assertEqual(15, Rect.area())
+
+    def test_str(self):
+        """Test for __str__()"""
+        Rect = Rectangle(4, 10, 1, 6, 1)
+        s = "[Rectangle] (1) 1/6 - 4/10"
+        self.assertEqual(s, str(Rect))
+
+    """def test_display(self):
+        Testing rectangle display
+        rect = Rectangle(1, 2 , 0, 1)
+        output = TestRectangle_stdout.capture_stdout(rect, "display")
+        self.assertEqual("\n#\n #\n", output.getvalue())"""
+
+    def test_update(self):
+        """Testing for upadte function
+        no values
+        one arg
+        two args
+        three args
+        four args
+        5 args
+        Respectively
+        """
+        rect = Rectangle(10, 10, 10, 10, 1)
+        rect.update()
+        self.assertEqual("[Rectangle] (1) 10/10 - 10/10", str(rect))
+
+        rect.update(4)
+        self.assertEqual("[Rectangle] (4) 10/10 - 10/10", str(rect))
+
+        rect.update(3, 4)
+        self.assertEqual("[Rectangle] (3) 10/10 - 4/10", str(rect))
+
+        rect.update(3, 4, 8)
+        self.assertEqual("[Rectangle] (3) 10/10 - 4/8", str(rect))
+
+        rect.update(1, 2, 3, 4)
+        self.assertEqual("[Rectangle] (1) 4/10 - 2/3", str(rect))
+
+        rect.update(7, 2, 3, 4, 6)
+        self.assertEqual("[Rectangle] (7) 4/6 - 2/3", str(rect))
+
+    def test_update_kwargs(self):
+        """Testing update() with key/value argument
+        one arg
+        2 args
+        3 args
+        4 args
+        5 args
+        """
+        rect = Rectangle(10, 10, 10, 10, 1)
+        rect.update(id=4)
+        self.assertEqual("[Rectangle] (4) 10/10 - 10/10", str(rect))
+
+        rect.update(id =3, width=4)
+        self.assertEqual("[Rectangle] (3) 10/10 - 4/10", str(rect))
+
+        rect.update(id=3, width=4, height=8)
+        self.assertEqual("[Rectangle] (3) 10/10 - 4/8", str(rect))
+
+        rect.update(width=2, x=4, height=3,id=1)
+        self.assertEqual("[Rectangle] (1) 4/10 - 2/3", str(rect))
+
+        rect.update(id=7, width=9, height=11, x=1,y =3)
+        self.assertEqual("[Rectangle] (7) 1/3 - 9/11", str(rect))
+
 
 
 if __name__ == '__main__':
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -3,5 +3,89 @@
 
 import unittest
 from models.square import Square
+from models.base import Base
+
+class TestSquare(unittest.TestCase):
+    """Testing the class squareinstances"""
+
+    def test_args(self):
+        """ Tests on various args
+        size
+        2 args
+        3 args
+        4 args
+        1st arg a string
+        2nd is not an int
+        3rd arg not an int
+        size = 0
+        x is -ve
+        y is -ve
+        respectively
+        """
+        self.assertIsInstance(Square(4), Base)
+        sq = Square(3, 2)
+        self.assertEqual(sq.size, 3)
+        sq = Square(1, 2, 3)
+        self.assertEqual(sq.x, 2)
+        sq = Square(1, 2, 4, 7)
+        self.assertEqual(sq.id, 7)
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square("1")
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(8, "3")
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(8, 7, "3")
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Square(0)
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            Square(2, -1)
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Square(2, 9, -1)
+
+    def test_update(self):
+        """Testing for update function
+        no values
+        one arg
+        two args
+        three args
+        four args
+        5 args
+        Respectively
+        """
+        sq = Square(10, 10, 10, 1)
+        sq.update()
+        self.assertEqual("[Square] (1) 10/10 - 10", str(sq))
+
+        sq.update(4)
+        self.assertEqual("[Square] (4) 10/10 - 10", str(sq))
+
+        sq.update(3, 4)
+        self.assertEqual("[Square] (3) 10/10 - 4", str(sq))
+
+        sq.update(3, 4, 8)
+        self.assertEqual("[Square] (3) 8/10 - 4", str(sq))
+
+        sq.update(7, 2, 3, 4)
+        self.assertEqual("[Square] (7) 3/4 - 2", str(sq))
 
 
+    def test_update_kwargs(self):
+        """Testing update() with key/value argument
+        one arg
+        2 args
+        3 args
+        4 args
+        5 args
+        """
+        sq = Square(10, 10, 10, 1)
+        sq.update(id=4)
+        self.assertEqual("[Square] (4) 10/10 - 10", str(sq))
+
+        sq.update(id=3, size=4)
+        self.assertEqual("[Square] (3) 10/10 - 4", str(sq))
+
+        sq.update(size=2, x=4,id=1)
+        self.assertEqual("[Square] (1) 4/10 - 2", str(sq))
+
+        sq.update(id=7, size=9, x=1,y =3)
+        self.assertEqual("[Square] (7) 1/3 - 9", str(sq))
