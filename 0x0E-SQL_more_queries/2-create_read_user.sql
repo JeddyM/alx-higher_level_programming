4ec67e51a024ab27633dfec19225acf657174f3f
Jeddy Mweru <jeddymweru@gmail.com>
2022-09-08 12:38:42 +0000

    More SQL queries

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- user has only SELECT privilege
+-- if db exists script should not fail
+-- if user already exits script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'local';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- user has only SELECT privilege
+-- if db exists script should not fail
+-- if user already exits script should not fail
+CREATE DATABASE hbtn_0d_2
+CREATE USER user_0d_2
+GRANT SELECT ON hbtn_0d_2.user_0d_2 TO 'user_0d_2'@'host';
