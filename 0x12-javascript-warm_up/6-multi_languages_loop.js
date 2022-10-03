50440cfcd557d8c91c9ab427b3ff54308c869c6e
Jeddy Mweru <jeddymweru@gmail.com>
2022-10-03 22:35:12 +0000

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const text = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (i = 0; i < 3; i++) {
 	console.log(text[i]);
 }
