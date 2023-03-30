790912e616f0dd4fc003ea0b5f3306e32e103fdb
Jeddy Mweru <jeddymweru@gmail.com>
2023-03-30 18:31:03 +0000

    Get request, delete and body size script

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+#Takes in URL sends GET request to the URL, and displays the body response
 curl -sL "$1"
