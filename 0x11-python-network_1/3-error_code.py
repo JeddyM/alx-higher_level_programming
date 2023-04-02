8512794413540c389971fb8043756f8f4f177aa6
Jeddy Mweru <jeddymweru@gmail.com>
2023-04-02 15:23:55 +0000

    Additing import urllib.error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 '''script that takes in a URL, sends a request and displays the body of
 the response (decoded in utf-8).
 '''
-import urllib.request
+from urllib import request, error
 import sys
 
 
